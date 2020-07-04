0be8bc88eb1993b9fed81f1a804f61f51cf73408
Elias Gomes da Silva <67754744+EliasGdS@users.noreply.github.com>
2020-07-04 13:43:24 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function converter() {
+    const nbin = document.getElementById('bnum').value;
+    if (nbin === '') return alert('É necessário inserir um número binário para a conversão.');
+    if (nbin.length > 8) return alert('Foi excedito o limite de 8 dígitos para a conversão.');
+
+    // nbin.split('').map((char) => {
+    //     if (char !== '0' && char !== '1') return alert('O valor inserido não é binário.');
+    // });
+
+    // A FUNÇÃO ACIMA CORREPONDE A FUNÇÃO A BAIXO
+    
+    nbin.split('').map(function(char){
+        if (char !== '0' && char !== '1') return alert('O valor inserido não é binário.');
+        else{
+            const ndec = parseInt(nbin, 2);
+            document.getElementById('dnum').value = ndec;
+            return true;
+        }
+    });
+
+}
